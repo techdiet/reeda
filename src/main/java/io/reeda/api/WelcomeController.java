7e8fcd8a683a8250701d8d59fee65cbb9b4232bb
Donkross360 <mart2great@yahoo.com>
2023-06-17 22:35:42 +0100

    REED-4-wrap-response-in-ResponseEntity-and-specify-the-HttpStatus-for-welcome-controller

--- src/main/java/io/reeda/api/WelcomeController.java
+++ src/main/java/io/reeda/api/WelcomeController.java
@@ -1,14 +1,19 @@
 package io.reeda.api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("api/welcome")
 public class WelcomeController {
     @GetMapping
-    public String welcome() {
-        return "Welcome To  Reeda Your Dream Library Management App!";
+    public ResponseEntity<String> welcome() throws IOException {
+
+        return new ResponseEntity<>("Welcome To  Reeda Your Dream Library Management App!", HttpStatus.OK);
     }
 }
