88c756922557f2d9d61af3f246386f5d9b1a94da
mejja <majorkiema.mk@gmail.com>
2023-06-20 16:39:09 +0300

    REED-6-Configured-Documentaion-Api-using-swagger

--- src/main/java/io/reeda/api/ApiApplication.java
+++ src/main/java/io/reeda/api/ApiApplication.java
@@ -2,12 +2,28 @@ package io.reeda.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
 
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@Configuration
+	public class OpenApiConfig {
+		@Bean
+		public OpenAPI usersMicroserviceOpenAPI() {
+			return new OpenAPI()
+					.info(new Info().title("Library system API Documentation")
+							.description("Library Management system using SpringBoot")
+							.version("1.0.0"));
+		}
 	}
 
 }
