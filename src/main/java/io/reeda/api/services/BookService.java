29748f93ad227577b5bad7546153fd9902613726
mejja <majorkiema.mk@gmail.com>
2023-07-02 21:05:51 +0300

    REED-11-create-a-method-in-the-service-class-to-find-a-book-to-the-database-by-its-isbn

--- src/main/java/io/reeda/api/services/BookService.java
+++ src/main/java/io/reeda/api/services/BookService.java
@@ -15,13 +15,15 @@ public class BookService {
         this.bookRepository = bookRepository;
     }
 
-    //Method to find a book by id
-    public Optional<Book> getSingleBook(long id) {
-        Optional<Book> bookContainer = bookRepository.findById(id);
-        if (bookContainer.isPresent()) {
-            return  bookContainer;
+    //Method to find a book by its isbn
+    public Optional<Book> searchByISBN(String isbn) {
+        Optional<Book> bookContainer = bookRepository.findBookByIsbn(isbn);
+
+        if (bookContainer.isPresent()){
+            return bookContainer;
         } else {
-            throw new IllegalArgumentException("Book not found: " + id);
+            throw new IllegalArgumentException("Book not found: " + isbn);
         }
     }
+
 }
