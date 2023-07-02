2d2ae38ec481c5706579bed21fac85908bce755b
enjesh <enjesh@gmail.com>
2023-07-02 16:39:56 +0300

    REED-7-create-a-method-in-the-service-class-to-add-a-book-to-the-database

--- src/main/java/io/reeda/api/services/BookService.java
+++ src/main/java/io/reeda/api/services/BookService.java
@@ -1,15 +1,20 @@
 package io.reeda.api.services;
 
+import io.reeda.api.models.Book;
 import io.reeda.api.repositories.BookRepository;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class BookService {
-    BookRepository bookRepository;
+    private BookRepository bookRepository;
 
     public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
+    }
+
+    public Book addbook(Book book) {
+        return bookRepository.save(book);
     }
 
 }
