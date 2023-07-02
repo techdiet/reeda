da2d912f2533cb6f10f3dd07f37846209ba23e71
mejja <majorkiema.mk@gmail.com>
2023-07-02 20:59:58 +0300

    REED-9-create-a-method-in-the-service-class-to-find-a-book-to-the-database-by-its-id

--- src/main/java/io/reeda/api/services/BookService.java
+++ src/main/java/io/reeda/api/services/BookService.java
@@ -1,7 +1,10 @@
 package io.reeda.api.services;
 
+import io.reeda.api.models.Book;
 import io.reeda.api.repositories.BookRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 
 @Service
@@ -12,4 +15,13 @@ public class BookService {
         this.bookRepository = bookRepository;
     }
 
+    //Method to find a book by id
+    public Optional<Book> getSingleBook(long id) {
+        Optional<Book> bookContainer = bookRepository.findById(id);
+        if (bookContainer.isPresent()) {
+            return  bookContainer;
+        } else {
+            throw new IllegalArgumentException("Book not found: " + id);
+        }
+    }
 }
