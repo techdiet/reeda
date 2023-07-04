efc6a0da2854a95c0d3a9ca8c20fe6c927baea8f
Donkross360 <mart2great@yahoo.com>
2023-07-04 11:58:05 +0100

    REED-13 Create a method in the controller as an endpoint for updating a book

--- src/main/java/io/reeda/api/controllers/BooksController.java
+++ src/main/java/io/reeda/api/controllers/BooksController.java
@@ -3,6 +3,7 @@ package io.reeda.api.controllers;
 import io.reeda.api.DTOs.BookDTO;
 import io.reeda.api.models.Book;
 import io.reeda.api.services.BookService;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +21,16 @@ public class BooksController {
 
     public BooksController(BookService bookService) {
         this.bookService = bookService;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody BookDTO updatedBook) {
+        try {
+            Book updated = bookService.updateBook(id, updatedBook);
+            return ResponseEntity.ok(updated);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
     }
 
 }
